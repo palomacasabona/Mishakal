eda3585779b368aa8e6b4cb22e8ff3f910bd8c3b
Paloma Casabona <p.casabona.a@gmail.com>
2024-11-25 23:51:58 +0100

    ajustes vista principal, logo y navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,32 @@
 </head>
 <body class="bg-gray-100">
 <!-- Barra de navegación superior -->
-<header class="bg-blue-500 text-white p-4">
+<header class="flex items-center justify-between p-4 bg-blue-700 text-white shadow-md">
     <div class="flex items-center">
         <div class="mr-4">
             <!-- Logo -->
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-8">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-16 sm:h-12 md:h-12 w-auto">
         </div>
-        <h1 class="text-xl font-bold">MISHAKAL</h1>
+        <h1 class="text-lg sm:text-xl font-bold">MISHAKAL</h1>
+    </div>
+    <!-- Menú para dispositivos grandes -->
+    <nav class="hidden md:block">
+        <ul class="flex space-x-4">
+            <li><a href="#" class="hover:text-gray-300">Log</a></li>
+            <li><a href="#" class="hover:text-gray-300">Registrarse</a></li>
+            <li><a href="#" class="hover:text-gray-300">Notificaciones</a></li>
+            <li><a href="#" class="hover:text-gray-300">Ajustes</a></li>
+
+        </ul>
+    </nav>
+    <!-- Botón hamburguesa para dispositivos pequeños -->
+    <div class="block md:hidden">
+        <button id="menu-toggle" class="focus:outline-none">
+            <!-- Icono de menú -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+        </button>
     </div>
 </header>
 

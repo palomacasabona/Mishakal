299727a8a10c959608a22c7b7bea963a95d47e82
Paloma Casabona <p.casabona.a@gmail.com>
2025-01-09 01:16:58 +0100

    plugin añadido de cloudflare

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
+
 
 
     <!-- VITE (CSS y JS) -->

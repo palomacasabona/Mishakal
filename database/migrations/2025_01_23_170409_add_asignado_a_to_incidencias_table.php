35a9b645267e3a9a7a4ce8c3b1f67a89aad86ae2
Paloma Casabona <p.casabona.a@gmail.com>
2025-01-23 18:18:46 +0100

    añadir column a incidencias table

---
+++ database/migrations/2025_01_23_170409_add_asignado_a_to_incidencias_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('incidencias', function (Blueprint $table) {
+            $table->unsignedBigInteger('asignado_a')->nullable()->after('estado'); // Añade la columna asignado_a
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('incidencias', function (Blueprint $table) {
+            $table->dropColumn('asignado_a'); // Elimina la columna en caso de rollback
+        });
+    }
+};

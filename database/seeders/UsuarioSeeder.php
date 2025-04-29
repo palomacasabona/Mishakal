ad4af7fa64c0a38f9aac6206787a377cf1772ab4
Paloma Casabona <p.casabona.a@gmail.com>
2025-04-30 00:26:12 +0200

    desarrollo show usuario con foto de perfil que ya se ve despues del seeder

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -11,14 +11,22 @@ class UsuarioSeeder extends Seeder
     public function run()
     {
         // Crear 200 usuarios nuevos con fotos aleatorias
-        Usuario::factory()->count(802)->create();
+        //Usuario::factory()->count(802)->create();
+        for ($i = 1; $i <= 200; $i++) {
+            $usuario = Usuario::find($i);
+            if ($usuario && empty($usuario->foto_perfil)) {
+                $number = rand(1, 70);
+                $usuario->foto_perfil = "https://i.pravatar.cc/150?img={$number}";
+                $usuario->save();
+            }
+        }
 
         // Ahora actualizamos SOLO los usuarios que no tienen foto
-        Usuario::whereNull('foto_perfil')->orWhere('foto_perfil', '')->get()->each(function ($usuario) {
-            $number = rand(1, 70);
-            $usuario->foto_perfil = "https://i.pravatar.cc/150?img={$number}";
-            $usuario->save();
-        });
+        //Usuario::whereNull('foto_perfil')->orWhere('foto_perfil', '')->get()->each(function ($usuario) {
+            //$number = rand(1, 70);
+            //$usuario->foto_perfil = "https://i.pravatar.cc/150?img={$number}";
+            //$usuario->save();
+        //});
     }
 
 }

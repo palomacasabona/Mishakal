78112b533400313e81be0acc880f3ea5835158b4
Paloma Casabona <p.casabona.a@gmail.com>
2025-05-02 16:30:46 +0200

    subimos ultimos cambios por si aca

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,5 +24,18 @@
         </form>
     </div>
 </div>
+{{--MODAL DE LOGUEARSE SI NO ESTAS CONECTADO PARA VER INCIDENCIAS--}}
+@if (session('error'))
+    <!-- Modal -->
+    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div class="bg-white p-6 rounded shadow-md max-w-sm w-full text-center">
+            <h2 class="text-lg font-semibold mb-2 text-red-600">Acceso restringido</h2>
+            <p class="text-sm text-gray-700 mb-4">{{ session('error') }}</p>
+            <button onclick="this.parentElement.parentElement.remove()" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                Cerrar
+            </button>
+        </div>
+    </div>
+@endif
 </body>
 </html>
